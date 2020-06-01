711cefe1f3d8deb62c987232ac7637a078942b4d
Vondreii <Sharlene.VonDrehnen@outlook.com>
2020-06-02 00:13:20 +1000

    Added individual post pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,6 +29,11 @@ import { HowToOrganiseYourWebBrowsingComponent } from './blog/organisation/2020/
 import { TipsToAvoidProcrastinatingComponent } from './blog/organisation/2020/tips-to-avoid-procrastinating/tips-to-avoid-procrastinating.component';
 import { TipsToBetterOrganiseYourComputerComponent } from './blog/organisation/2020/tips-to-better-organise-your-computer/tips-to-better-organise-your-computer.component';
 import { WaysToBeMoreOrganisedComponent } from './blog/organisation/2020/ways-to-be-more-organised/ways-to-be-more-organised.component';
+import { HowToMakeTheMostOutOfYourFirstYearAtUniComponent } from './blog/uni/2020/how-to-make-the-most-out-of-your-first-year-at-uni/how-to-make-the-most-out-of-your-first-year-at-uni.component';
+import { CreatingANewThemeInHugoComponent } from './blog/website/2020/creating-a-new-theme-in-hugo/creating-a-new-theme-in-hugo.component';
+import { MakingASimpleStaticWebsiteWithHtmlAndCssComponent } from './blog/website/2020/making-a-simple-static-website-with-html-and-css/making-a-simple-static-website-with-html-and-css.component';
+import { JobsForIntrovertsThatDontNeedMuchCommunicationComponent } from './blog/wellbeing/2020/jobs-for-introverts-that-dont-need-much-communication/jobs-for-introverts-that-dont-need-much-communication.component';
+import { MakingLifeGoalsComponent } from './blog/wellbeing/2020/making-life-goals/making-life-goals.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -69,13 +74,24 @@ const routes: Routes = [
   { path: 'blog/organisation/tips-to-avoid-procrastinating', component: TipsToAvoidProcrastinatingComponent },
   { path: 'blog/organisation/tips-to-better-organise-your-computer', component: TipsToBetterOrganiseYourComputerComponent },
   { path: 'blog/organisation/ways-to-be-more-organised', component: WaysToBeMoreOrganisedComponent },
+
+  // Blog - Uni
+  { path: 'blog/uni/how-to-make-the-most-out-of-your-first-year-of-uni', component: HowToMakeTheMostOutOfYourFirstYearAtUniComponent },
+
+  // Blog - Website
+  { path: 'blog/website/creating-a-new-theme-in-hugo', component: CreatingANewThemeInHugoComponent },
+  { path: 'blog/website/making-a-simple-static-website-with-html-and-css', component: MakingASimpleStaticWebsiteWithHtmlAndCssComponent },
+
+  // Blog - Wellbeing
+  { path: 'blog/wellbeing/jobs-for-introverts-that-dont-need-much-communication', component: JobsForIntrovertsThatDontNeedMuchCommunicationComponent },
+  { path: 'blog/wellbeing/making-life-goals', component: MakingLifeGoalsComponent },
 ]
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})
   ], 
   exports: [RouterModule]
 })
