f66fa9d8451651ce28c03bc27339eed6effb0e40
Vondreii <Sharlene.VonDrehnen@outlook.com>
2020-06-08 22:40:19 +1000

    Fixed search bar and started uni post

--- src/app/blog/minimalism/minimalism-main/minimalism-main.component.ts
+++ src/app/blog/minimalism/minimalism-main/minimalism-main.component.ts
@@ -12,4 +12,31 @@ export class MinimalismMainComponent implements OnInit {
   ngOnInit() {
   }
 
+  searchbar() {
+    var input, filter, ul, li, a, i, txtValue, noResults;
+    var resultsExist = false;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    noResults = document.getElementById("no-results");
+    li = ul.getElementsByTagName("section");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("div")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+            resultsExist = true;
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+    if(resultsExist)
+    {
+      noResults.style.display = "none";
+    }
+    else
+    {
+      noResults.style.display = "block";
+    }
+  }
 }
