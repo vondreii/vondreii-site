54f7bc3cae4745024bd9cff71c05d8ef30516456
Vondreii <Sharlene.VonDrehnen@outlook.com>
2020-06-17 17:57:50 +1000

    WIP photography to DB

--- src/app/components/photography/blue-mountains/blue-mountains.component.ts
+++ src/app/components/photography/blue-mountains/blue-mountains.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-blue-mountains',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blue-mountains.component.css']
 })
 export class BlueMountainsComponent implements OnInit {
+  photography: any;
 
-  constructor() { }
+  col1: [];
+  col2: [];
+  col3: [];
+  col4: [];
+
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
+    this.photography = this.db.collection('photography').valueChanges({ idField: 'id' });
+    this.photography.forEach(location => {
+      location.forEach(column => {
+        this.col1 = column.col1;
+        this.col2 = column.col2;
+        this.col3 = column.col3;
+        this.col4 = column.col4;
+        // console.log(column.col2)
+        // console.log(column.col3)
+        // console.log(column.col4)
+      });
+    });
   }
 
 }
